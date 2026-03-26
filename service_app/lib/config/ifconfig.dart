f2de0484fa197157c8d9907b0542e2fdbb4587e0
Sandeep Swain <swainsandeep67@gmail.com>
2026-03-26 14:27:35 +0530

    Add API keys to gitignore - remove sensitive files from tracking

--- service_app/lib/config/ifconfig.dart
+++
@@ -1,9 +0,0 @@
-class ApiConfig {
-  static const String deepSeekApiKey = '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';
-  
-  // Or load from different sources
-  static String getDeepSeekApiKey() {
-    // You can add logic here to get API key from different sources
-    return deepSeekApiKey;
-  }
-}
